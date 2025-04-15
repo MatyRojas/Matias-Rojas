71637734db9f8578ba0888d02f38afd8eba5de3f
Matias Rojas <maty.rojas2002@gmail.com>
2025-04-15 11:54:19 -0400

    Primera commit

---
+++ hola.py
@@ -0,0 +1,31 @@
+# Declaracion de Variables
+print("hola mundo")
+nombre = input(print("Cual es tu nombre"))
+edad = input(print("Cual es tu edad"))
+# Ejemplo de Contatenacion
+print("Hola", nombre, "tu edad es", edad)
+print("Hola {} tu edad es {}".format(nombre, edad))
+
+#Ejemplos de suma
+
+print("Vamos a hacer una suma")
+
+num1 = int(input(print("Dame el primer numero")))
+num2 = int(input(print("Dame el segundo numero")))
+
+suma = num1 + num2
+
+print("El resultado de la suma es",suma)
+
+#Sin print
+
+print("Vamos a hacer una suma")
+
+num1 = int(input("Dame el primer numero "))
+num2 = int(input("Dame el segundo numero "))
+suma = num1 + num2
+
+print("El resultado de la suma es",suma)
+
+#
+
