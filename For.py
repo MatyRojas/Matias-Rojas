331d3277256ea769d79dec6b40654520d098f85c
Maty <maty.rojas2002@gmail.com>
2025-04-22 11:00:42 -0400

    este faltaba

---
+++ For.py
@@ -0,0 +1,13 @@
+# Estructura y uso de for:
+
+for i in range(5): #Lista del 0 al 4 (ya que son cinco numeros)
+    print(i)
+
+for i in range(5): #Ahora cuenta del 1 al 5
+    print(i+1)
+
+for i in range(1,6): #Es lo mismo escrito de otra manera
+    print(i)
+             
+for i in range(6,1): #Cuenta regresiva
+    print(i)
